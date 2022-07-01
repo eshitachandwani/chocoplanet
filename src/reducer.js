5db23b36bb21c3252e84376a2efabb610d9eafc3
eshitachandwani <chandwanieshita06@gmail.com>
2022-07-01 14:27:40 +0530

    edited

--- src/reducer.js
+++ src/reducer.js
@@ -4,10 +4,13 @@ export const initialState = {
 }
 
 // Selector
-// export const getBasketTotal = (basket) =>
+// export const GetBasketTotal = (basket) =>
 //   basket?.reduce((amount, item) => item.price + amount, 0)
 
-const reducer = (state, action) => {
+export const getBasketTotal = (basket) =>
+  basket.reduce((amount, item) => item.price + amount, 0)
+
+function reducer(state, action) {
   console.log(action)
   switch (action.type) {
     case 'ADD_TO_BASKET':
@@ -15,6 +18,38 @@ const reducer = (state, action) => {
         ...state,
         basket: [...state.basket, action.item],
       }
+
+    case 'EMPTY_BASKET':
+      return {
+        ...state,
+        basket: [],
+      }
+
+    case 'REMOVE_FROM_BASKET':
+      const index = state.basket.findIndex(
+        (basketItem) => basketItem.id === action.id,
+      )
+      let newBasket = [...state.basket]
+
+      if (index >= 0) {
+        newBasket.splice(index, 1)
+      } else {
+        console.warn(
+          `Cant remove product (id: ${action.id}) as its not in basket!`,
+        )
+      }
+
+      return {
+        ...state,
+        basket: newBasket,
+      }
+
+    case 'SET_USER':
+      return {
+        ...state,
+        user: action.user,
+      }
+
     default:
       return state
   }
